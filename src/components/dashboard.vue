c9212ee8629fd0644ffda585d8535deac7d188a1
Zhenyuan <zhenyuan.ye@hotmail.com>
2018-11-23 16:01:26 +1100

    dashboard page design

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -1,11 +1,29 @@
 <template>
+  <div class="dashboard">
+    <h1>{{ msg }} {{firstname}} {{lastname}}</h1>
+    <!--TODO: the logic of users-->
+    <div>
+      <router-link v-bind:to="'/settings'"><button>Settings</button></router-link>
+    </div>
+    <div>
+      <router-link v-bind:to="'/'"><button>Logout</button></router-link>
+    </div>
+  </div>
 
 </template>
 
 <script>
-  export default {
-    name: 'dashboard'
+export default {
+  name: 'dashboard',
+  data () {
+    return {
+      msg: 'Welcome,',
+      // TODO: change them to the first&last name of the user
+      firstname: 'Jane',
+      lastname: 'Dow'
+    }
   }
+}
 </script>
 
 <style scoped>
