da19f02f54cc130d6de09a24b58abf309c5123a9
Zhenyuan <zhenyuan.ye@hotmail.com>
2018-11-23 16:08:52 +1100

    settings page design

--- src/components/settings.vue
+++ src/components/settings.vue
@@ -1,11 +1,27 @@
 <template>
-
+  <div class="settings">
+    <h1>{{ msg }}</h1>
+    new password
+    <input v-model="new_password" placeholder="Your new password" type="password">
+    <br />
+    new password confirmation
+    <input v-model="confirmed_new_password" placeholder="Confirm your new password" type="password">
+    <br /><br />
+    <router-link v-bind:to="'/settings'"><button>Change</button></router-link>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'settings'
+export default {
+  name: 'settings',
+  data () {
+    return {
+      msg: 'Welcome to the settings page!',
+      new_password: null,
+      confirmed_new_password: null
+    }
   }
+}
 </script>
 
 <style scoped>
