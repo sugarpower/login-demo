77bfbdc92a816677b1c719fd8090234f61aeb55a
Zhenyuan <zhenyuan.ye@hotmail.com>
2018-11-26 12:02:02 +1100

    applied form

--- src/components/signup.vue
+++ src/components/signup.vue
@@ -3,18 +3,18 @@
     <h1>{{ msg }}</h1>
     <!--noted that validation is not added on this page-->
     <!--TODO: validation-->
-    <div>
+    <form id="signupform">
       first name
-      <input v-model="firstname" placeholder="Your first name">
+      <input v-model="firstname" placeholder="Your first name" type="text">
       <br />
       last name
-      <input v-model="lastname" placeholder="Your last name">
+      <input v-model="lastname" placeholder="Your last name" type="text">
       <br />
       email
-      <input v-model="email" placeholder="Your email">
+      <input v-model="email" placeholder="Your email" type="email">
       <br />
       phone number
-      <input v-model="phone" placeholder="Your phone number">
+      <input v-model="phone" placeholder="Your phone number" type="text">
       <br />
       password
       <input v-model="password" placeholder="Your password" type="password">
@@ -22,18 +22,26 @@
       password confirmation
       <input v-model="confirmed_password" placeholder="Your password" type="password">
       <br />
-
-      <!--just a demo button-->
-      <!--TODO: logic will be set up later-->
-      <router-link v-bind:to="'/'"><button>Signup</button></router-link>
-    </div>
+      <input type="submit" value="Signup">
+    </form>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'signup'
+export default {
+  name: 'signup',
+  data () {
+    return {
+      msg: 'Welcome to the Signup page!',
+      firstname: null,
+      lastname: null,
+      email: null,
+      phone: null,
+      password: null,
+      confirmed_password: null
+    }
   }
+}
 </script>
 
 <style scoped>
