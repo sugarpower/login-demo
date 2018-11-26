3efb9e584f16fb8358e2f3191a90adb108cdcd22
Zhenyuan <zhenyuan.ye@hotmail.com>
2018-11-26 16:36:45 +1100

    sign up by firebase

--- src/components/signup.vue
+++ src/components/signup.vue
@@ -3,37 +3,73 @@
     <h1>{{ msg }}</h1>
     <!--noted that validation is not added on this page-->
     <!--TODO: validation-->
-    <div>
+    <form id="signupform" v-on:submit.prevent="addUser">
       first name
-      <input v-model="firstname" placeholder="Your first name">
+      <input v-model="newUser.firstname" placeholder="Your first name" type="text">
       <br />
       last name
-      <input v-model="lastname" placeholder="Your last name">
+      <input v-model="newUser.lastname" placeholder="Your last name" type="text">
       <br />
       email
-      <input v-model="email" placeholder="Your email">
+      <input v-model="newUser.email" placeholder="Your email" type="email">
       <br />
       phone number
-      <input v-model="phone" placeholder="Your phone number">
+      <input v-model="newUser.phone" placeholder="Your phone number" type="text">
       <br />
       password
-      <input v-model="password" placeholder="Your password" type="password">
+      <input v-model="newUser.password" placeholder="Your password" type="password">
       <br />
       password confirmation
-      <input v-model="confirmed_password" placeholder="Your password" type="password">
+      <input v-model="newUser.confirmed_password" placeholder="Your password" type="password">
       <br />
-
-      <!--just a demo button-->
-      <!--TODO: logic will be set up later-->
-      <router-link v-bind:to="'/'"><button>Signup</button></router-link>
-    </div>
+      <input type="submit" value="Signup">
+    </form>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'signup'
+import firebase from 'firebase'
+var config = {
+  apiKey: 'AIzaSyDJK9PrpiGlE4Q4FD75_hQOJmBPhrVaKE8',
+  authDomain: 'login-demo-5fc1e.firebaseapp.com',
+  databaseURL: 'https://login-demo-5fc1e.firebaseio.com',
+  projectId: 'login-demo-5fc1e',
+  storageBucket: 'login-demo-5fc1e.appspot.com',
+  messagingSenderId: '800753801457'
+}
+firebase.initializeApp(config)
+
+var usersRef = firebase.database().ref('users')
+
+export default {
+  name: 'signup',
+  data () {
+    return {
+      newUser: {
+        firstname: '',
+        lastname: '',
+        email: '',
+        phone: '',
+        password: '',
+        confirmed_password: ''
+      }
+    }
+  },
+  firebase: {
+    users: usersRef
+  },
+  methods: {
+    addUser: function () {
+      usersRef.push(this.newUser)
+      this.newUser.firstname = ''
+      this.newUser.lastname = ''
+      this.newUser.email = ''
+      this.newUser.phone = ''
+      this.newUser.password = ''
+      this.newUser.confirmed_password = ''
+    }
   }
+}
 </script>
 
 <style scoped>
